2d7a3cd8bf5fe988993db1a42e2de4d146c74f02
AzamatMedetbekov <azamatmedetbekovv@gmail.com>
2024-10-10 12:22:08 +0900

    create notice controller and module

---
+++ src/notice/notice.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { NoticeController } from './notice.controller';
+
+
+@Module({
+  controllers: [NoticeController],
+})
+export class NoticeModule {}
